b34a87ec186e44c5cf54d8883cc24971a05a92f2
JT17458 <36880656+JT17458@users.noreply.github.com>
2018-03-06 10:21:28 +0100

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Car {
+	String brand;
+	boolean isNew;
+	int milleage;
+	double price;
+
+	Car(){
+		System.out.println("Inicjacja z nowego konstruktora");
+	}
+
+}
---
+++ Program.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Program {
+
+
+	public static void main(String[]args){
+		
+		Car car = new Car();
+		car.brand = "BMW";
+		car.isNew = true;
+		car.milleage = 0;
+		car.price = 100000.99;
+		
+		System.out.println("Marka: " + car.brand);
+		System.out.println("Nowy: " + car.isNew);
+		System.out.println("Przebieg: " + car.milleage);
+		System.out.println("Cena: " + car.price);
+	
+	}
+}
